d19306e257bb4dbf63ee2a4e2f447cc57048988f
jerome B <jerome@monocycle.(none)>
2013-02-12 23:48:17 +0100

    gestion des erreurs structure multicommande

--- Py_affiche.py
+++ Py_affiche.py
@@ -12,15 +12,34 @@ import argparse
 import serial
 import time
 
+"""
+order
+		0x01 Select a programm pre store on display DL to ram
+		0x11 Select a programm pre store on display DL to flash
+
+		0x02 Select several programms pre store on display DL to ram
+		0x12 Select several programms pre store on display DL to flash
+		
+		0x03 Insert text to display DL to ram
+		0x13 Insert text to display DL to flash
+		
+		0x04 Exit extern control play prog sent by computer DL to ram
+		0x14 Exit extern control play prog sent by computer DL to flash
+		
+		0x05 Insert text to display (upgrade) DL to ram
+		0x15 Insert text to display (upgrade) DL to flash
+
+"""
 
 parser = argparse.ArgumentParser()
 parser.add_argument("texte", help="display the string you use here")
+parser.add_argument("-v", "--verbosity", help="increase output verbosity", action="count")
 parser.add_argument("-p", "--port", help="selected port - default /dev/ttyUSB0")
-parser.add_argument("-id", "--displayID", help=" Display ID - default 1", type=int)
+parser.add_argument("-i", "--displayID", help=" Display ID - default 1", type=int)
 parser.add_argument("-o", "--order", help="Order to executute - default Insert text to display")
-parser.add_argument("-fc","--fontcolor",help="color to display 1: red 2: green 3: yellow - default red", type=int) 
+parser.add_argument("-c", "--fontcolor",help="color to display 1: red 2: green 3: yellow - default red", type=int) 
 parser.add_argument("-s", "--speed",help="display speed, 0 -> 255", type=int) 
-parser.add_argument("-m","--mode",help="display mode 01: shift left 02: Instant 03: shift up 05: scroll left", type=int)
+parser.add_argument("-m", "--mode",help="display mode 01: shift left 02: Instant 03: shift up 05: scroll left", type=int)
 parser.add_argument("-f", "--font",help="font to use :0 font8(5*7) 2 font16 3 font24 4 font32",type=int)
 
 
@@ -55,7 +74,7 @@ else:
 if args.mode:
 	DisplayMode = args.mode
 else:
-	DisplayMode = 2 # instant display
+	DisplayMode = 5 # scroll left
 
 if args.font:
 	DisplayFont = args.font
@@ -65,17 +84,21 @@ else:
 	else:
 		DisplayFont = 0
 
-print"Port:      ",DisplayPort
-print"Order:     ",DisplayOrder
-print"FontColor: ",DisplayFontColor
-print"Speed:     ",DisplaySpeed
-print"Mode:      ",DisplayMode
-print"font:      ",DisplayFont
-
-print args.texte
-
+if args.verbosity >= 2:
+	print"Port:      ",DisplayPort
+	print"Order:     ",DisplayOrder
+	print"FontColor: ",DisplayFontColor
+	print"Speed:     ",DisplaySpeed
+	print"Mode:      ",DisplayMode
+	print"font:      ",DisplayFont
+	print "----------------"
+	print args.texte
+	print "----------------"
+	
 Start_code = chr(0xa5) +chr(0xed)
+
 E_C_insert = chr(0x0d)+ chr(0x0a) 	# end code for insert
+E_C_Cmd = chr (0xae)				# end code for commands
 
 # ser = serial.Serial (DisplayPort, 57600, timeout=1)
 ser = serial.Serial()
@@ -86,65 +109,85 @@ ser.stopbits=1
 ser.parity=serial.PARITY_EVEN
 ser.timeout=1
 
-
-cmd_send_display = Start_code + chr(0x10) + chr(DisplayID) + chr(DisplayOrder) + chr(DisplayFontColor) + chr(DisplaySpeed) + chr(DisplayMode) + chr(DisplayFont)
-cmd_send_display = cmd_send_display + args.texte
-cmd_send_display = cmd_send_display + E_C_insert
-verifCode=0
-for i in range(1, len (cmd_send_display)):
-	verifCode = verifCode + ord(cmd_send_display[i])
-
-verif_high = (verifCode & 0xFF00)/0xFF
-# remplace 0xa5 0xae 0xaa
-if verif_high == 0xa5:
-	verifcode_str = chr(0xaa) + chr (0x5)
-elif verif_high == 0xae:
-	verifcode_str = chr(0xaa) + chr (0xe)
-elif verif_high == 0xaa:
-	verifcode_str = chr(0xaa) + chr (0xa)
-else:
-	verifcode_str = chr(verif_high)
+if DisplayOrder == 0x01:
+	if args.verbosity >= 1:
+		print "Order 0x01"
+elif DisplayOrder == 0x11:
+	if args.verbosity >= 1:
+		print "Order 0x11"
+elif DisplayOrder == 0x02:
+	if args.verbosity >= 1:
+		print "Order 0x02"
+elif DisplayOrder == 0x12:
+	if args.verbosity >= 1:
+		print "Order 0x12"
+elif DisplayOrder == 0x03:
+	if args.verbosity >= 1:
+		print "Order 0x03"
+elif DisplayOrder == 0x13:
+	if args.verbosity >= 1:
+		print "Order 0x13"
+elif DisplayOrder == 0x04:
+	if args.verbosity >= 1:
+		print "Order 0x04"
+elif DisplayOrder == 0x14:
+	if args.verbosity >= 1:
+		print "Order 0x14"
+elif DisplayOrder == 0x05:
+	if args.verbosity >= 1:
+		print "Order 0x05"
+	cmd_send_display = Start_code + chr(0x10) + chr(DisplayID) + chr(DisplayOrder) + chr(DisplayFontColor) + chr(DisplaySpeed) + chr(DisplayMode) + chr(DisplayFont)
+	cmd_send_display = cmd_send_display + args.texte
+	cmd_send_display = cmd_send_display + E_C_insert
+	verifCode=0
+	for i in range(1, len (cmd_send_display)):
+		verifCode = verifCode + ord(cmd_send_display[i])
 	
-verif_low = verifCode & 0xFF
-# remplace 0xa5 0xae 0xaa
-if verif_low == 0xa5:
-	verifcode_str = verifcode_str + chr(0xaa) + chr (0x5)
-elif verif_low == 0xae:
-	verifcode_str = verifcode_str + chr(0xaa) + chr (0xe)
-elif verif_low == 0xaa:
-	verifcode_str = verifcode_str + chr(0xaa) + chr (0xa)
-else:
-	verifcode_str = verifcode_str + chr(verif_low)
+	verif_high = (verifCode & 0xFF00)/0xFF
+	# remplace 0xa5 0xae 0xaa
+	if verif_high == 0xa5:
+		verifcode_str = chr(0xaa) + chr (0x5)
+	elif verif_high == 0xae:
+		verifcode_str = chr(0xaa) + chr (0xe)
+	elif verif_high == 0xaa:
+		verifcode_str = chr(0xaa) + chr (0xa)
+	else:
+		verifcode_str = chr(verif_high)
+		
+	verif_low = verifCode & 0xFF
+	# remplace 0xa5 0xae 0xaa
+	if verif_low == 0xa5:
+		verifcode_str = verifcode_str + chr(0xaa) + chr (0x5)
+	elif verif_low == 0xae:
+		verifcode_str = verifcode_str + chr(0xaa) + chr (0xe)
+	elif verif_low == 0xaa:
+		verifcode_str = verifcode_str + chr(0xaa) + chr (0xa)
+	else:
+		verifcode_str = verifcode_str + chr(verif_low)
+		
+	cmd_send_display = cmd_send_display + verifcode_str + chr (0xae)
 
-
-
-cmd_send_display = cmd_send_display + verifcode_str + chr (0xae)
-
-print "----------------"
-
+elif DisplayOrder == 0x15:
+	print "Order 0x15"
 ## print cmd_send_display
-"""
-for i in range(0,len(cmd_send_display)):
-	print hex(ord(cmd_send_display[i]))
-	i = i +1
-"""
+if args.verbosity == 3:
+	for i in range(0,len(cmd_send_display)):
+		print hex(ord(cmd_send_display[i]))
+		i = i +1
  
 try:
 	ser.open()
 except serial.SerialException:
 	print"erreur sur l'ouverture du port"
+	exit(1)
 
-if ser.isOpen:
-	print"send command"
+if ser.isOpen():
+	if args.verbosity >= 2:
+		print"send command"
 	ser.write(cmd_send_display)
 	ser.flush()
 	s = ser.read(100)
-	print "----------------"
-	"""
-	print s
-	for i in range(0,len(s)):
-		print hex(ord(s[i]))
-		i = i +1
-	print "----------------"
-	"""
+	if args.verbosity >= 2:
+		print "----------------"
 	ser.close
+	exit(0)
